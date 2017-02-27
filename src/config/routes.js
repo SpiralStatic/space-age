0b0b95ea1c4e5044459c52efae0de0b581e3db68
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-02-27 14:38:27 +0000

    Corrected user.js to users.js in api controllers

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var serveController = require('../controllers/serve.js');
+var apiUsersController = require('../controllers/api/users.js');
 
 router.route('/')
     .get(serveController.index);
