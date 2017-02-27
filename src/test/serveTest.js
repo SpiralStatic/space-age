893303ee2866d9044dd5d6c1c5a3503546f14462
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-02-27 13:29:28 +0000

    Added mocha-chai testing for serve.js

--- src/test/serveTest.js
+++ src/test/serveTest.js
@@ -1,7 +1,23 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var app = require('../app');
+var app = require('../index');
 var should = chai.should();
 var expect = require('chai').expect;
 
 chai.use(chaiHttp);
+
+describe('Serve', () => {
+    // Test the /GET route
+    describe('/GET', () => {
+        it('It should GET the single web page', (done) => {
+            chai.request(app)
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.html;
+                    res.text.should.match(/Space Age/);
+                    done();
+                });
+        });
+    });
+});
