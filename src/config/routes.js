9c0909126fb8b3c2628bbbd6bb4157ccc1d351de
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-02-27 14:40:17 +0000

    Added user routes to router

--- src/config/routes.js
+++ src/config/routes.js
@@ -7,4 +7,12 @@ var apiUsersController = require('../controllers/api/users.js');
 router.route('/')
     .get(serveController.index);
 
+router.route('/api/users')
+    .get(apiUsersController.index)
+    .create(apiUsersController.create);
+
+router.route('/api/users/:id')
+    .get(apiUsersController.show)
+    .get(apiUsersController.delete);
+
 module.exports = router;
