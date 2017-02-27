1ef21fd04df011c479f3e61f1c52a641f8c97183
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-02-27 11:51:41 +0000

    Added index.html, the backend main application file. Installed required npm packages

---
+++ src/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var layouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+
+var app = express();
+
+// Use public resource folder
+app.use(express.static('../public'));
+
+// Connect to the database
+mongoose.connect('mongodb://localhost/spaceage', function() {
+    console.log("Database is now connected");
+});
+
+// Body Parser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Method Override
+app.use(methodOverride(function(req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
+
+// Set the view engine to be ejs
+app.set('view engine', 'ejs');
+
+// Include express layouts middleware
+app.use(layouts);
+
+// Add the router
+app.use(routes);
+
+// Start server
+app.listen(3000, function() {
+    console.log("Server Started. Listening on port 3000");
+});
+
+module.exports = app;
