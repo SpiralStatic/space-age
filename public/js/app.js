ce9187390b413ed2ffe476956493c36dc79a8c7c
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-02-27 15:54:36 +0000

    Removed bower setup, transferred angular to libaries folder, installed angular-ui-router along with it, added home state

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,14 @@
 angular
-    .module('SpaceAgeApp', []);
+    .module('SpaceAgeApp', ['ui.router'])
+    .config(MainRouter);
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: '/states/home.html',
+        });
+
+    $urlRouterProvider.otherwise('/');
+}
