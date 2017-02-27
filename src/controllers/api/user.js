5d2cd8df127401ea7a05a3ddad3cde37ebfd279c
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-02-27 14:35:40 +0000

    Added the rest of the functions to the user.js controller. Show, Create and Delete

--- src/controllers/api/user.js
+++ src/controllers/api/user.js
@@ -2,13 +2,57 @@ var User = require('../../models/user.js');
 
 function indexUserFavorites(req, res) {
     User.findById(req.user.uid, function(err, user) {
-        if (err) return res.status(500).json({
-            error: err.message
-        });
+        ifNotFound();
+        elseError();
         res.status(200).json(user.favorites);
     });
 }
 
+function showUser(req, res) {
+    User.findById(req.user.uid, function(err, user) {
+        ifNotFound();
+        elseError();
+
+        res.status(200).json(user);
+    });
+}
+
+function createUser(req, res) {
+    User.create(req.body, function(err, user) {
+        ifNotFound();
+        elseError();
+
+        res.status(204).json(user);
+    });
+}
+
+function deleteUser(req, res) {
+    User.delete(req.user.uid, function(err, user) {
+        ifNotFound();
+        elseError();
+
+        res.status(204).json({
+            message: "Successful deletion"
+        });
+    });
+}
+
+function ifNotFound(user) {
+    if (!user) return res.status(404).json({
+        error: err.message,
+        message: "User not found"
+    });
+}
+
+function elseError(user) {
+    if (err) return res.status(500).json({
+        error: err.message
+    });
+}
+
 module.exports = {
-    index: indexUserFavorites
+    index: indexUserFavorites,
+    show: showUser,
+    create: createUser,
+    delete: deleteUser
 };
