80709b3a5727f44ea13949f2cf3719a5e47d520c
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-02-28 14:16:19 +0000

    Added Auth controller methods for registering and signing in

--- public/js/controllers/auth.ctrl.js
+++ public/js/controllers/auth.ctrl.js
@@ -4,6 +4,43 @@ angular
 
 function AuthController(Auth, $scope, LoginService) {
     var self = this;
+
+    self.createUser = function() {
+        Auth.$createUserWithEmailAndPassword(self.email, self.password)
+            .then(function(user) {
+                resetCredentials();
+                
+            })
+            .catch(function(error) {
+                self.error = error;
+            });
+    };
+
+    self.signIn = function() {
+        Auth.$signInWithEmailAndPassword(self.email, self.password)
+            .then(function(user) {
+                resetCredentials();
+            })
+            .catch(function(error) {
+                self.error = error;
+            });
+    };
+
+    self.signOut = function() {
+        Auth.$signOut();
+        $state.go('home');
+    };
+
+    Auth.$onAuthStateChanged(function(user) {
+        console.log(user);
+        self.user = user;
+    });
+
+    function resetCredentials() {
+        self.email = "";
+        self.password = "";
+    }
+
     $scope.loginHidden = LoginService.sharedObject.loginHidden;
 
     self.loginShow = function() {
