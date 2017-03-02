7f34d44e13d9d30b9acf2ce2014be49ea4805d26
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-03-02 13:47:45 +0000

    Added weather icons to launch directive, installed weather-icons to use

--- public/js/filters/filters.js
+++ public/js/filters/filters.js
@@ -12,7 +12,67 @@ app.filter('stripTimeWindow', function() {
     return function(input) {
         var date = new Date(input).toDateString().substring(4);
         var time = new Date(input).toLocaleTimeString();
-        time = time.substring(0,4).concat(time.substring(7));
+        time = time.substring(0, 4).concat(time.substring(7));
         return date + ' ' + time;
+    };
+});
+
+app.filter('convertIcon', function() {
+    return function(input) {
+        var iconReference = [{
+            apiIcon: ['01d'],
+            icon: 'wi-day-sunny'
+        }, {
+            apiIcon: ['02d'],
+            icon: 'wi-day-cloudy'
+        }, {
+            apiIcon: ['03d', '03n'],
+            icon: 'wi-cloud'
+        }, {
+            apiIcon: ['04d', '04n'],
+            icon: 'wi-cloudy'
+        }, {
+            apiIcon: ['09d'],
+            icon: 'wi-day-showers'
+        }, {
+            apiIcon: ['10d'],
+            icon: 'wi-day-rain'
+        }, {
+            apiIcon: ['11d'],
+            icon: 'wi-day-thunderstorm'
+        }, {
+            apiIcon: ['13d'],
+            icon: 'wi-day-snow'
+        }, {
+            apiIcon: ['50d'],
+            icon: 'wi-day-fog'
+        }, {
+            apiIcon: ['01n'],
+            icon: 'wi-day-sunny'
+        }, {
+            apiIcon: ['02n'],
+            icon: 'wi-night-alt-cloudy'
+        }, {
+            apiIcon: ['09n'],
+            icon: 'wi-night-showers'
+        }, {
+            apiIcon: ['10n'],
+            icon: 'wi-night-alt-rain'
+        }, {
+            apiIcon: ['11n'],
+            icon: 'wi-night-alt-thunderstorm'
+        }, {
+            apiIcon: ['13n'],
+            icon: 'wi-night-alt-snow'
+        }, {
+            apiIcon: ['50n'],
+            icon: 'wi-day-sunny'
+        }];
+        var output = iconReference.filter(function(icon, i) {
+            ///console.log(icon.apiIcon.indexOf(input));
+            if(icon.apiIcon.indexOf(input) !== -1) return i;
+        });
+        console.log(output);
+        return output[0].icon;
     };
 });
